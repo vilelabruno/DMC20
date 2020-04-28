7fafa315cfcc586727ae40c6943350a55791a87a
Rafael Machado <rafafsmachado@gmail.com>
2020-04-27 22:02:22 -0300

    evaluation metric version 1 DONE

--- xgboostModel.py
+++ xgboostModel.py
@@ -4,15 +4,24 @@ import xgboost as xgb
 def gradient(predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
     '''Compute the gradient squared log error.'''
     y = dtrain.get_label()
-    return (np.log1p(predt) - np.log1p(y)) / (predt + 1)
+    price = dtrain.get_weight()
+    
+    predt[(predt-y) < 0] = (1/(y+1))-(8*(y-predt)*np.power(np.log1p(predt),2)*np.power(np.log1p(price), 2))
+    predt[(predt-y) > 0] = (1/(y+1))-(2*(y-predt)*np.power(np.log1p(predt),2)*np.power(np.log1p(price), 2))
+    
+    return predt
 
 def hessian(predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
     '''Compute the hessian for squared log error.'''
     y = dtrain.get_label()
-    return ((-np.log1p(predt) + np.log1p(y) + 1) /
-            np.power(predt + 1, 2))
+    price = dtrain.get_weight()
 
-def squared_log(predt: np.ndarray,
+    predt[(predt-y) < 0] = (-1/np.power(y+1, 2))-(8*np.power(np.log1p(predt),2)*np.power(np.log1p(price), 2))
+    predt[(predt-y) > 0] = (-1/np.power(y+1, 2))-(2*np.power(np.log1p(predt),2)*np.power(np.log1p(price), 2))
+
+    return predt
+
+def weighted_squared_log(predt: np.ndarray,
                 dtrain: xgb.DMatrix) -> Tuple[np.ndarray, np.ndarray]:
     '''Squared Log Error objective. A simplified version for RMSLE used as
     objective function.
