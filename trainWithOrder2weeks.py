b3965cd0d54e3b502290be67e96b51f7b10fa1de
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-05-13 23:44:25 -0300

    remove for and created lstm

--- trainWithOrder2weeks.py
+++ trainWithOrder2weeks.py
@@ -10,18 +10,15 @@ orders["weekNumber"] = -1
 orders["weekPromotion"] = 0
 aux = infos.promotion.str.split(",", expand=True).reset_index() 
 aux["index"] = aux.index + 1  
-orders = orders.merge(aux, how="left", left_on="itemID", right_on="index")
 
-orders[0] = pd.to_datetime(orders[0])
-orders[1] = pd.to_datetime(orders[1])
-orders[2] = pd.to_datetime(orders[2])
-for i in range(0,13):
-    orders["weekNumber"][(orders["time"] > (pd.to_datetime("2018-01-01") + timedelta(days=(14*i)))) & (orders["time"] < (pd.to_datetime("2018-01-01") + timedelta(days=(14*(i+1)))))] = i
-    orders["weekPromotion"][((orders[0] > (pd.to_datetime("2018-01-01") + timedelta(days=(14*i)))) & (orders[0] < (pd.to_datetime("2018-01-01") + timedelta(days=(14*(i+1)))))) & (orders["weekNumber"] == i)] = 1
-    orders["weekPromotion"][((orders[1] > (pd.to_datetime("2018-01-01") + timedelta(days=(14*i)))) & (orders[1] < (pd.to_datetime("2018-01-01") + timedelta(days=(14*(i+1)))))) & (orders["weekNumber"] == i)] = 1
-    orders["weekPromotion"][((orders[2] > (pd.to_datetime("2018-01-01") + timedelta(days=(14*i)))) & (orders[2] < (pd.to_datetime("2018-01-01") + timedelta(days=(14*(i+1)))))) & (orders["weekNumber"] == i)] = 1
+orders["dayDate"] = orders["time"].dt.strftime("%Y-%m-%d")
 
-gp = orders.groupby(["itemID", "weekNumber"]).agg({"order": "sum", "weekPromotion": "sum"})
+orders = orders.merge(aux, how="left", left_on=["itemID", "dayDate"], right_on=["index", 0])
+orders = orders.merge(aux, how="left", left_on=["itemID", "dayDate"], right_on=["index", 1])
+orders = orders.merge(aux, how="left", left_on=["itemID", "dayDate"], right_on=["index", 2])
+
+#%%
+gp = orders.groupby(["itemID", "dayDate"]).agg({"order": "sum", "weekPromotion": "sum"})
 #gp = orders.groupby(["weekNumber"]).agg({"order": sum})#this lines contains the plot for orders                                                  
 #gp.reset_index(inplace=True)                           #this lines contains the plot for orders                      
 #gp = gp.sort_values(by=["weekNumber"])                 #this lines contains the plot for orders                              
