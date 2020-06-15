0a8fd743a92d9f8d3724eceeafba2067b82ce10c
vilela <a16042@bcc.unifal-mg.edu.br>
2020-06-15 15:49:33 -0300

    6

--- mountDFAllDays.py
+++ mountDFAllDays.py
@@ -6,14 +6,18 @@ items = pd.read_csv("data/items.csv", sep="|")
 infos = pd.read_csv("data/infos.csv", sep="|")
 
 orders["time"] = pd.to_datetime(orders["time"])
+orders["date"] = orders["time"].dt.date
+orders = orders.groupby(["itemID", "date"]).agg({"order": "sum", "salesPrice": "sum"})
+orders.reset_index(inplace=True)
+aux = pd.DataFrame(columns=["a", "b", "c", "d"])
+for j in range(7848, 9156):
+    for i in range(0, 182):
+        if len(orders[(orders["date"] == (pd.to_datetime("2018-01-01") + timedelta(days=(i)))) & (orders["itemID"] == j)]) == 0:
+            aux.loc[len(aux)] = [j, (pd.to_datetime("2018-01-01") + timedelta(days=(i))), 0, 0]
+    print(len(aux))
+    
+#orderInfos = orders.merge(infos, how='left', on='itemID')
+#
+#fullData = orderInfos.merge(items, how='left', on='itemID')
 
-for j in range(1, 10464):
-    if len(orders[((orders["time"] >= (pd.to_datetime("2018-01-01") + timedelta(days=(i)))) & (orders["time"] < (pd.to_datetime("2018-01-01") + timedelta(days=(i+1))))) & (orders["itemID"] == j)]) == 0:
-        orders.loc[len(orders)] = [(pd.to_datetime("2018-01-01") + timedelta(days=(i))), -1, j, 0, 0]
-        #print(len(orders))
-
-orderInfos = orders.merge(infos, how='left', on='itemID')
-
-fullData = orderInfos.merge(items, how='left', on='itemID')
-
-fullData.to_csv('data/trainAllDays.csv', index=False)
+aux.to_csv('data/9.csv', index=False)
